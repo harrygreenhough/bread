681b4d29f3667da2650de711cb8b7dd66ae9907b
Harry <hgreenhough@gmail.com>
2023-12-02 10:23:39 +1100

    Add project files.

---
+++ recipes/BasicSourdough.cs
@@ -0,0 +1,28 @@
+﻿namespace recipes
+{
+    public class BasicSourdough
+    {
+        private const int _baseRecipeTotal = 1685;
+        
+        private const decimal _wholemealFlourRatio = 150/_baseRecipeTotal;
+        private const decimal _plainFlourRatio = 750/_baseRecipeTotal;
+        private const decimal _saltRatio = 25/_baseRecipeTotal;
+        private const decimal _starterRatio = 200/_baseRecipeTotal;
+        private const decimal _waterRatio = 560/_baseRecipeTotal;
+        
+        public BasicSourdough(int totalWeight)
+        {
+            TotalWeight = totalWeight;
+
+            WholemealFlour = Math.Ceiling(totalWeight * _wholemealFlourRatio);
+        }
+
+        public int TotalWeight { get; set; }
+
+        public decimal WholemealFlour { get; private set; }
+        public decimal PlainFlour { get; private set; }
+        public decimal Salt { get; private set; }
+        public decimal Starter { get; private set; }
+        public decimal Water { get; private set; }
+    }
+}
